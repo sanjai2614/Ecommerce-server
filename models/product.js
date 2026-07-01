e80a32ab2f76c928ac6b0db2751f78446552bd92
Sanjai <sanjai63698@gmail.com>
2026-07-01 12:50:15 +0530

    implement catgeory model

--- models/product.js
+++ models/product.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
-const productSchema=new mongoose.Schema({
+const productSchema = new mongoose.Schema({
     id: Number,
     image: String,
     name: String,
+    category: String,
     price: Number,
     discountPrice: Number,
     description: String,
     rating: String
-});
+}, { timestamps: true });
 
-export default mongoose.model("Products",productSchema);
+export default mongoose.model("Products", productSchema);
